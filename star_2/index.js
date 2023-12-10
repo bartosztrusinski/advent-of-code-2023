eaa7cf991f33a88faeba8d5c34a5fbed6faab99b
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-12-10 19:55:57 +0100

    get second star

---
+++ star_2/index.js
@@ -0,0 +1,49 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+const numbers = [];
+const allDigitsPattern = /\d/g;
+const spelledOutDigits = {
+  zero: '0',
+  one: '1',
+  two: '2',
+  three: '3',
+  four: '4',
+  five: '5',
+  six: '6',
+  seven: '7',
+  eight: '8',
+  nine: '9',
+};
+
+const rl = readline.createInterface({
+  input: fs.createReadStream(path.resolve(__dirname, 'input.txt')),
+  crlfDelay: Infinity,
+});
+
+const run = () => {
+  rl.on('line', (input) => {
+    const number = getNumberFromInput(input);
+    numbers.push(number);
+  }).on('close', () => {
+    console.log(numbers.reduce((sum, num) => sum + num, 0));
+  });
+};
+
+const getNumberFromInput = (input) => {
+  const noSpelledDigitsInput = replaceWordsWithDigits(input);
+  const digitsOnly = noSpelledDigitsInput.match(allDigitsPattern);
+  const firstDigit = digitsOnly[0];
+  const lastDigit = digitsOnly.at(-1);
+
+  return Number(`${firstDigit}${lastDigit}`);
+};
+
+const replaceWordsWithDigits = (inputString) => {
+  return Object.entries(spelledOutDigits).reduce((input, [word, digit]) => {
+    return input.replaceAll(word, `${word}${digit}${word}`);
+  }, inputString);
+};
+
+run();
