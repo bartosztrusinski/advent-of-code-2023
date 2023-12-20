c1a36d0d31d8b0f8c55c1fbffd5db7c8a448305d
Bartosz Trusinski <bartosztrusinski@gmail.com>
2023-12-20 15:56:33 +0100

    earn seventh star

---
+++ calendar/day_4/part_1.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const readline = require('readline');
+const path = require('path');
+
+const cardPointSums = [];
+
+const rl = readline.createInterface({
+  input: fs.createReadStream(path.resolve(__dirname, '../../inputs/day_4.txt')),
+  crlfDelay: Infinity,
+});
+
+const run = () => {
+  rl.on('line', (inputLine) => {
+    const [winningNumbers, playedNumbers] = getCardNumbers(inputLine);
+
+    const cardHitCount = playedNumbers.reduce(
+      (hitCount, playedNumber) =>
+        winningNumbers.includes(playedNumber) ? hitCount + 1 : hitCount,
+      0
+    );
+
+    const cardPoints = cardHitCount === 0 ? 0 : 2 ** (cardHitCount - 1);
+
+    cardPointSums.push(cardPoints);
+  }).on('close', () => {
+    console.log(cardPointSums.reduce((sum, cardPoints) => sum + cardPoints, 0));
+  });
+};
+
+const getCardNumbers = (inputLine) => {
+  const startIndex = inputLine.indexOf(':') + 1;
+  const splitIndex = inputLine.indexOf('|');
+  const winningNumbers = inputLine
+    .substring(startIndex, splitIndex)
+    .split(' ')
+    .filter(Number)
+    .map(Number);
+  const playedNumbers = inputLine
+    .substring(splitIndex + 1)
+    .split(' ')
+    .filter(Number)
+    .map(Number);
+
+  return [winningNumbers, playedNumbers];
+};
+
+run();
