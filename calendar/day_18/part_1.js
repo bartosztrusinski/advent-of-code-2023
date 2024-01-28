c555dd532e4feae5cc4ec89934c36dfec3221e1d
Bartosz Trusinski <bartosztrusinski@gmail.com>
2024-01-28 15:59:55 +0100

    checkpoint

--- calendar/day_18/part_1.js
+++ calendar/day_18/part_1.js
@@ -5,11 +5,19 @@ const day = 18;
 const digPlan = [];
 
 const onLineRead = (inputLine) => {
-  const [direction, metersToDig, trenchColor] = inputLine.split(' ');
-  digPlan.push([direction, Number(metersToDig), trenchColor.slice(1, -1)]);
+  const [direction, metersToDig] = inputLine.split(' ');
+  digPlan.push([direction, Number(metersToDig)]);
 };
 
 const onClose = () => {
+  const [width, height, center] = getDigSiteDimensions();
+  const digSiteGrid = createDigSiteGrid(width, height, center);
+  const totalMetersDig = computeTotalMetersDig(digSiteGrid);
+
+  console.log(totalMetersDig);
+};
+
+const getDigSiteDimensions = () => {
   const startPosition = [0, 0];
   let maxLeft = 0;
   let maxRight = 0;
@@ -47,30 +55,29 @@ const onClose = () => {
     }
   }
 
-  // console.log(`Width: ${maxRight - maxLeft} Height: ${maxUp - maxDown}`);
-  // console.log(`Max left: ${maxLeft} Max right: ${maxRight}`);
-  // console.log(`Max up: ${maxUp} Max down: ${maxDown}`);
+  const width = maxRight - maxLeft + 1;
+  const height = maxUp - maxDown + 1;
+  const center = [Math.abs(maxLeft), Math.abs(maxUp)];
 
-  const width = maxRight - maxLeft;
-  const height = maxUp - maxDown;
+  return [width, height, center];
+};
 
-  const grid = Array(height * 2)
+const createDigSiteGrid = (width, height, center) => {
+  const grid = Array(height)
     .fill()
-    .map(() => Array(width * 2).fill('.'));
+    .map(() => Array(width).fill('.'));
 
-  let [x, y] = [170, height];
+  let [x, y] = center;
+
+  console.log(`width: ${width}, height: ${height} center: ${center}`);
 
   for (let i = 0; i < digPlan.length; i++) {
     const [direction, metersToDig] = digPlan[i];
 
-    // console.log(
-    //   `INDEX: ${i} Direction: ${direction} Meters to dig: ${metersToDig} X: ${x} Y: ${y}`
-    // );
-
     switch (direction) {
       case 'R':
         for (let j = x; j < x + metersToDig; j++) {
-          grid[y][j] = '#';
+          grid[y] ? (grid[y][j] = '#') : console.log(`x: ${x}, y: ${y}`);
         }
         x += metersToDig;
         break;
@@ -95,164 +102,103 @@ const onClose = () => {
     }
   }
 
-  // const idx = 200;
+  return grid.filter((line) => line.includes('#'));
+};
 
-  // console.log(
-  //   `first # at
-  //   ${grid[idx].findIndex((cell) => cell === '#')}
-  //   | last # at ${grid[idx].findLastIndex((cell) => cell === '#')}
-  //   RESULT : ${
-  //     grid[idx].findIndex((cell) => cell === '#') === -1
-  //       ? 0
-  //       : grid[idx].findLastIndex((cell) => cell === '#') -
-  //         grid[idx].findIndex((cell) => cell === '#') +
-  //         1
-  //   }`
-  // );
+const computeTotalMetersDig = (grid) => {
+  return grid
+    .map((row, rowIndex) => {
+      const startDigIndices = row
+        .map((cell, index) =>
+          cell === '#' && row[index - 1] && row[index - 1] === '.' ? index : -1
+        )
+        .filter((index) => index !== -1);
 
-  //73111 too high
+      const endDigIndices = row
+        .map((cell, index) =>
+          cell === '#' && row[index + 1] && row[index + 1] === '.' ? index : -1
+        )
+        .filter((index) => index !== -1);
 
-  const gridWithoutEmptyLines = grid.filter((line) => line.includes('#'));
+      let isInside = false;
 
-  const gridWithDigCount = gridWithoutEmptyLines.map((row, i) => {
-    const startDigIndices = row
-      .map((cell, idx) => (cell === '#' && row[idx - 1] === '.' ? idx : -1))
-      .filter((idx) => idx !== -1);
+      const points1 = [];
+      const points2 = [];
 
-    const endDigIndices = row
-      .map((cell, idx) => (cell === '#' && row[idx + 1] === '.' ? idx : -1))
-      .filter((idx) => idx !== -1);
+      startDigIndices.forEach((startIndex, index) => {
+        const endIndex = endDigIndices[index];
 
-    let isInside = false;
+        if (startIndex === endIndex) {
+          isInside = !isInside;
+          points1.push(startIndex);
+          return;
+        }
 
-    const points1 = [];
-    const points2 = [];
+        // console.log(`index: ${index} start: ${startIndex} end: ${endIndex} `);
+        // console.log(
+        //   grid[index - 1]?.[startIndex],
+        //   grid[index + 1]?.[endIndex],
+        //   grid[index - 1]?.[startIndex] === grid[index + 1]?.[endIndex]
+        // );
+        // console.log(
+        //   grid[index + 1]?.[startIndex],
+        //   grid[index - 1]?.[endIndex],
+        //   grid[index + 1]?.[startIndex] === grid[index - 1]?.[endIndex]
+        // );
 
-    startDigIndices.forEach((startIndex, index) => {
-      const endIndex = endDigIndices[index];
+        console.log(rowIndex);
+        console.log(
+          grid[rowIndex - 1]?.join('').slice(startIndex - 1, endIndex + 2)
+        );
+        console.log(row?.join('').slice(startIndex - 1, endIndex + 2));
+        console.log(
+          grid[rowIndex + 1]?.join('').slice(startIndex - 1, endIndex + 2)
+        );
 
-      if (startIndex === endIndex) {
-        isInside = !isInside;
-        points1.push(startIndex);
-        return;
+        if (
+          (grid[rowIndex - 1] &&
+            grid[rowIndex + 1] &&
+            grid[rowIndex - 1]?.[startIndex] ===
+              grid[rowIndex + 1]?.[endIndex]) ||
+          grid[rowIndex + 1]?.[startIndex] === grid[rowIndex - 1]?.[endIndex]
+        ) {
+          isInside = !isInside;
+          points1.push([startIndex, endIndex]);
+          return;
+        }
+
+        if (!isInside) {
+          points2.push([startIndex, endIndex]);
+        }
+      });
+
+      let points1Sum = 0;
+
+      console.log(
+        `index ${rowIndex} startDigIndices: ${startDigIndices} endDigIndices: ${endDigIndices} \nPOINTS1 ${points1} POINTS2 ${points2}`
+      );
+      console.log(points1);
+      console.log(points2);
+
+      for (let i = 0; i < points1.length; i += 2) {
+        console.log(points1[i], points1[i + 1]);
+        const start =
+          typeof points1[i] === 'number' ? points1[i] : points1[i][0];
+        const end =
+          typeof points1[i + 1] === 'number'
+            ? points1[i + 1]
+            : points1[i + 1][1];
+
+        points1Sum += end - start + 1;
       }
 
-      if (
-        gridWithoutEmptyLines[i - 1]?.[startIndex] ===
-          gridWithoutEmptyLines[i + 1]?.[endIndex] ||
-        gridWithoutEmptyLines[i + 1]?.[startIndex] ===
-          gridWithoutEmptyLines[i - 1]?.[endIndex]
-      ) {
-        isInside = !isInside;
-        points1.push([startIndex, endIndex]);
-        return;
-      }
+      const points2Sum = points2
+        .map((point) => point[1] - point[0] + 1)
+        .reduce((a, b) => a + b, 0);
 
-      if (!isInside) {
-        points2.push([startIndex, endIndex]);
-      }
-      // if start === end TRUE
-      // if start^ and endV OR startV and end^ TRUE
-      // FALSE
-    });
-
-    // const points1 = points.filter((point) => typeof point === 'number');
-    // .reduce((sum, point, index) => {
-    //   if (index % 2 === 0) {
-    //     return sum;
-    //   }
-    let points1Sum = 0;
-    for (let i = 0; i < points1.length; i += 2) {
-      const start = typeof points1[i] === 'number' ? points1[i] : points1[i][0];
-      const end =
-        typeof points1[i + 1] === 'number' ? points1[i + 1] : points1[i + 1][1];
-
-      points1Sum += end - start + 1;
-    }
-
-    // if start === end TRUE
-    // if start^ and endV OR startV and end^ TRUE
-    // FALSE
-
-    // if (point === points[index + 1]) {
-    //   return sum;
-    //
-
-    //   return sum + (point - points[index - 1] + 1);
-    // }, 0);
-    const points2Sum = points2
-      .filter((point) => Array.isArray(point))
-      .map((point) => point[1] - point[0] + 1)
-      .reduce((a, b) => a + b, 0);
-
-    // loop
-    // if start === end => ITS A POINT
-    // if start !== end => ITS A LINE
-    // if line start and end go in the same direction => COUNT LINE LENGTH
-    // if line start and end go in the opposite direction => COUNT LINE LENGTH
-
-    // #........#............#............#.........#.#.........#.........#
-    // #........#............###..........#.........###.........######....#  <= THIS LINE
-    // #........#..............#..........#..........................#....#
-    //                                                          ^ line changes inside to outside and vice versa
-    // ^ point  ^ point       ^ line changes inside to outside and vice versa
-    //                                    ^ point   ^line but doesnt change inside/outside
-
-    // SO OUTPUT SHOULD COUNT LIKE THAT
-    // ##########............##############.........###.........###########
-    // ##########............##############.........###.........###########  <= THIS LINE
-    // ##########..............############..........................######
-
-    // if (i === idx) {
-    //   console.log(`INDEX ${idx}`);
-    //   console.log(`START: ${startDigIndices}`);
-    //   console.log(`END: ${endDigIndices}`);
-    //   console.log(
-    //     startDigIndices.reduce((sum, start, index) => {
-    //       if (index % 2 === 1 || !endDigIndices[index + 1]) {
-    //         return sum;
-    //       }
-
-    //       return sum + (endDigIndices[index + 1] - start + 1);
-    //     }, 0)
-    //   );
-    //   console.log(
-    //     row
-    //       .filter(
-    //         (_, index) =>
-    //           index >= startDigIndices[0] &&
-    //           index <= endDigIndices[endDigIndices.length - 1]
-    //       )
-    //       .join('')
-    //   );
-
-    //   console.log(`POINTS1: ${points1} POINTS 2: ${points2}`);
-    //   console.log(`POINTS1 SUM: ${points1Sum} POINTS 2 SUM: ${points2Sum}`);
-    // }
-
-    // return startDigIndices.reduce((sum, start, index) => {
-    //   if (index % 2 === 1 || !endDigIndices[index + 1]) {
-    //     return sum;
-    //   }
-
-    //   return sum + (endDigIndices[index + 1] - start + 1);
-    // }, 0);
-
-    return points1Sum + points2Sum;
-  });
-
-  const totalMetersDig = gridWithDigCount.reduce((a, b) => a + b, 0);
-
-  console.log(totalMetersDig);
-
-  // const l = 200;
-
-  // console.log(
-  //   gridWithoutEmptyLines
-  //     .filter((_, i) => i >= 199 && i <= 201)
-  //     .map((row, i) => i + row.join('').slice(l, l + 180))
-  //     .join('\n')
-  // );
+      return points1Sum + points2Sum;
+    })
+    .reduce((sum, rowMetersDig) => sum + rowMetersDig, 0);
 };
 
 module.exports = () => run(day, onLineRead, onClose);
